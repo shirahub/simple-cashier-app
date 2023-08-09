01cfc5ca096dd7847977f029b2dd5fdd9f29bf8e
Shirleen <shirleen.adriana@gmail.com>
2023-08-09 09:43:44 +0700

    implement basic SOLID and OOP

--- modules/cart.py
+++ modules/cart.py
@@ -1,3 +1,13 @@
-def add_item_to_cart(cart, item_id, item_quantity):
-    cart[item_id] = item_quantity
-    return cart
+class Cart:
+    def __init__(self, items=None):
+        if items is None:
+            self.items = {}
+        else:
+            self.items = items
+
+    def add_item_to_cart(self, item_id, item_quantity):
+        prev_qty = self.items.get(item_id, 0)
+        self.items[item_id] = prev_qty + item_quantity
+
+    def get_items(self):
+        return self.items
--- usecases/shopping.py
+++ usecases/shopping.py
@@ -2,25 +2,28 @@ import modules.items
 import modules.cart
 import utils.inputs
 
-cart = {}
+cart = modules.cart.Cart()
+
 
 def validate_item_id(id):
     product = modules.items.get_product_by_id(id)
     return product
+
 
 def add_item_to_cart():
     global cart
     item_id = utils.inputs.input_int("Input ID dari Produk: ", "Periksa kembali Input ID Produk Anda.")
     product = validate_item_id(item_id)
     item_qty = utils.inputs.input_int("Input Jumlah dari Produk: ", "Periksa kembali Input Jumlah Produk Anda.")
-    cart = modules.cart.add_item_to_cart(cart, item_id, item_qty)
+    cart.add_item_to_cart(item_id, item_qty)
     see_items_in_cart()
 
 
 def see_items_in_cart():
+    global cart
     total = 0
 
-    for k, v in cart.items():
+    for k, v in cart.get_items().items():
         product = [x for x in modules.items.get_products() if x.id == k][0]
         total = total + v * product.price
         print("Product: ", product.name, "Quantity: ", v, "Total: ", v * product.price)
